a8e85146b2ca5789989286e90ec0a8d92bfd4133
tantico <tantico@hotmail.com>
2022-04-23 22:13:43 +0300

    first1

---
+++ src/main/kotlin/repo.kt
@@ -0,0 +1,101 @@
+import java.util.concurrent.atomic.AtomicInteger
+
+data class Post (
+        val id: UInt = 0U,
+        val owner_id: UInt,
+        val from_id: UInt,
+        val created_by: UInt,
+        val date: UInt,
+        val text: String,
+        val reply_owner_id: UInt = 0U,
+        val reply_post_id: UInt = 0U,
+        val friends_only: Boolean = false,
+        val comments: Comments = Comments(),
+        val copyright: Copyright = Copyright(1U,1U, "", ""),
+        val likes: Likes = Likes(),
+        val reposts: Reposts = Reposts(),
+        val views: Views = Views(),
+        val post_type: String = "post",
+        val signer_id: UInt = 0U,
+        val can_pin: Boolean = true,
+        val can_delete: Boolean = true,
+        val can_edit: Boolean = true,
+        val is_pinned: Boolean = false,
+        val marked_as_ads: Boolean = false,
+        val is_favorite: Boolean = false,
+        val donut: Donut = Donut(),
+        val postponed_id: UInt = 0U,
+        )
+
+data class Comments (
+        val count: UInt = 0U,
+        val can_post: Boolean = true,
+        val groups_can_post: Boolean = true,
+        val can_close: Boolean = true,
+        val can_open: Boolean = true
+        )
+
+data class Copyright (
+        val id: UInt,
+        val link: UInt,
+        val name: String,
+        val type: String
+        )
+
+data class Likes (
+        val count: UInt = 0U,
+        val user_likes: Boolean = true,
+        val can_like: Boolean = true,
+        val can_publish: Boolean = true
+        )
+
+data class Reposts (
+        val count: UInt = 0U,
+        val user_reposted: Boolean = true
+        )
+
+data class Views (
+        val count: UInt = 0U
+        )
+
+data class Donut (
+        val is_donut: Boolean = false,
+        val paid_duration: UInt = 0U,
+        val can_publish_free_copy: Boolean = false,
+        val edit_mode: String = "all",
+        val duration: UInt = 0U
+        )
+
+object WallService {
+        private var counter: AtomicInteger = AtomicInteger()
+        private var posts = emptyArray<Post>()
+
+        fun add(post: Post): Post {
+                val newPost = post.copy(id = counter.incrementAndGet().toUInt());
+                posts += newPost
+                return newPost
+        }
+
+        fun update(post: Post): Boolean {
+            for ((index, oldPost) in posts.withIndex())
+                if (oldPost.id == post.id) {
+                   val newPost = post.copy(
+                                   from_id = oldPost.from_id,
+                                   date = oldPost.date)
+
+                   posts[index] = newPost
+                   return true
+                }
+
+            return false
+        }
+
+        fun get(index: Int): Post {
+            return posts[index]
+        }
+
+        fun clear() {
+            posts = emptyArray()
+            counter.set(0)
+        }
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,45 @@
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Before
+    fun setUp() {
+        WallService.clear()
+
+        val post = Post(0U,1U,1U,1U,1U,"test-1")
+        WallService.add(post)
+    }
+
+    @Test
+    fun add() {
+        val post = WallService.add(Post(0U,2U,2U,2U,2U,"test-2"))
+
+        val expected = 2U
+        val actual = post.id
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun update_existing() {
+        val updPost = WallService.get(0).copy(text = "updated")
+
+        val expected = true
+        val actual = WallService.update(updPost)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun update_no_existing() {
+        val updPost = Post(3U,3U,3U,3U,3U,"test-3")
+
+        val expected = false
+        val actual = WallService.update(updPost)
+
+        assertEquals(expected, actual)
+    }
+}
